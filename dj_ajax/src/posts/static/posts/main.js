80b0cde2e6c0c35ce0f11c658412b4f6c9112e4d
Ahmed A <163431268+ahmed-almouna@users.noreply.github.com>
2025-04-02 00:34:28 -0400

    Display posts through ajax calls

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,1 +1,38 @@
 console.log('hello world')
+
+const helloWolrdBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
+
+
+
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response.text)
+        helloWolrdBox.textContent = response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
+
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br>
+            `
+        });
+    },
+    error: function(error){
+        console.log(error)
+    }
+})
